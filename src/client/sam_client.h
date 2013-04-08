bc23ab2fa0ae1fba2b2c916ca681bdf337f05c04
meeshmusic@gmail.com <meeshmusic@gmail.com>
2013-04-08 20:21:07 +0000

    incrementing version to 0.3.6 (should have been done on last checkin)

--- src/client/sam_client.h
+++ src/client/sam_client.h
@@ -21,7 +21,7 @@
 namespace sam
 {
     
-static const char* VERSION_STRING = "0.3.5";
+static const char* VERSION_STRING = "0.3.6";
 static const unsigned int SAC_DEFAULT_TIMEOUT = 10; // in seconds
 
 /**
