2aa662061cd424eef80d583b6dd8a7293fde180d
meeshmusic@gmail.com <meeshmusic@gmail.com>
2013-04-27 01:31:55 +0000

    Fixed bug with SAC_DEFAULT_TIMEOUT that was in seconds rather than milliseconds

--- src/client/sam_client.h
+++ src/client/sam_client.h
@@ -23,8 +23,8 @@ namespace sam
     
 static const int VERSION_MAJOR = 0;
 static const int VERSION_MINOR = 5;
-static const int VERSION_PATCH = 1;
-static const unsigned int SAC_DEFAULT_TIMEOUT = 10; // in seconds
+static const int VERSION_PATCH = 2;
+static const unsigned int SAC_DEFAULT_TIMEOUT = 10000; // in milliseconds
 
 /**
  * @enum StreamingAudioType
