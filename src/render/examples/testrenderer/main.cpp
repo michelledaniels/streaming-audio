8971721c39e4af8d5ea65c515674f197d7a0bd41
meeshmusic@gmail.com <meeshmusic@gmail.com>
2014-01-23 23:04:48 +0000

    start to renderering library and example/test renderer using library.  need to work out TCP vs UDP communication inside of SAM before SAM can talk to renderers over TCP

---
+++ src/render/examples/testrenderer/main.cpp
@@ -0,0 +1,184 @@
+/**
+ * @file testrenderer/main.cpp
+ * testrenderer: command-line application using JACK for testing SAM renderer library
+ * @author Michelle Daniels
+ * @date January 2014
+ * @copyright UCSD 2014
+ * @license New BSD License: http://opensource.org/licenses/BSD-3-Clause
+ */
+
+#include <getopt.h>
+#include <signal.h>
+
+#include <QtCore/QCoreApplication>
+
+#include "samrenderer.h"
+
+using namespace sam;
+
+void print_help()
+{
+    printf("Usage: [testrenderer --name or -n JACK client name]\n");
+    printf("--ip or -i SAM ip\n");
+    printf("--port or -p SAM OSC port\n");
+    printf("--oscport or -o renderer OSC port\n");
+    printf("--channels or -c number of input channels (JACK input ports)\n");
+    printf("\nExample usage:\n");
+    printf("testrenderer -n \"TestRenderer\" -i \"127.0.0.1\" -p 7770 -o 0 -c 64\n");
+    printf("\n");
+}
+
+void signalhandler(int sig)
+{
+    qDebug("signal handler called: signal %d", sig);
+    if (sig == SIGINT || sig == SIGTERM)
+    {
+        qDebug("telling app to exit");
+        QCoreApplication::exit(0);
+    }
+}
+void stream_added_callback(SamRenderStream& stream, void* arg)
+{
+    qWarning("testrenderer: stream added with ID = %d, %d channels", stream.id, stream.numChannels);
+}
+
+void stream_removed_callback(int id, void* arg)
+{
+    qWarning("testrenderer: stream with ID = %d removed", id);
+}
+
+void position_changed_callback(int id, int x, int y, int width, int height, int depth, void* arg)
+{
+    qWarning("testrenderer: stream with ID = %d position changed to x = %d, y = %d, width = %d, height = %d, depth = %d", id, x, y, width, height, depth);
+}
+
+void type_changed_callback(int id, int type, int preset, void* arg)
+{
+    qWarning("testrenderer: stream with ID = %d type changed to %d, preset = %d", id, type, preset);
+}
+
+void render_disconnect_callback(void*)
+{
+    qWarning("testrenderer: lost connection with SAM, shutting down...");
+    QCoreApplication::quit();
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 9)
+    {
+        print_help();
+        exit(EXIT_SUCCESS);
+    }
+
+    QCoreApplication a(argc, argv);
+
+    SamRenderer renderer;
+
+    SamRenderParams params;
+    params.samIP = NULL;
+    params.samPort = 0;
+    params.replyIP = NULL;
+    params.replyPort = 0;
+
+    char* jackClientName = NULL;
+    int numChannels = 0;
+
+    // parse command-line parameters
+    while (true)
+    {
+        static struct option long_options[] = {
+            // These options set a flag.
+            {"name", required_argument, NULL, 'n'},
+            {"ip", required_argument, NULL, 'i'},
+            {"port", required_argument, NULL, 'p'},
+            {"oscport", required_argument, NULL, 'o'},
+            {"channels", required_argument, NULL, 'c'},
+            {NULL, 0, NULL, 0}
+        };
+
+        // getopt_long stores the option index here.
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "n:i:p:o:c:", long_options, &option_index);
+
+        // Detect the end of the options.
+        if (c == -1) break;
+
+        if (!optarg)
+        {
+            qWarning("c = %d, optarg NULL!", c);
+            exit(EXIT_FAILURE);
+        }
+        //qWarning("c = %d, optarg = %s", c, optarg);
+
+        switch (c)
+        {
+        case 'n':
+            jackClientName = optarg;
+            qWarning("setting JACK client name = %s", jackClientName);
+            break;
+
+        case 'i':
+            params.samIP = optarg;
+            qWarning("setting SAM IP = %s", params.samIP);
+            break;
+
+        case 'p':
+            params.samPort = atoi(optarg);
+            qWarning("setting SAM OSC port = %u", params.samPort);
+            break;
+
+        case 'o':
+            params.replyPort = atoi(optarg);
+            qWarning("setting renderer OSC port = %u", params.replyPort);
+            break;
+
+        case 'c':
+        {
+            int temp = atoi(optarg);
+            if (temp <=0)
+            {
+                qCritical("Number of channels must be at least 1");
+            }
+            numChannels = temp;
+            qWarning("setting number of channels = %u", numChannels);
+            break;
+        }
+        default:
+            print_help();
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    if (!jackClientName)
+    {
+        print_help();
+        exit(EXIT_FAILURE);
+    }
+
+    printf("finished parsing args");
+
+    signal(SIGINT, signalhandler);
+    signal(SIGTERM, signalhandler);
+
+    if (renderer.init(params) != SAMRENDER_SUCCESS)
+    {
+        qWarning("Couldn't initialize SamRenderer");
+        exit(EXIT_FAILURE);
+    }
+
+    if (renderer.start() != SAMRENDER_SUCCESS)
+    {
+        qWarning("Couldn't start SamRenderer");
+        exit(EXIT_FAILURE);
+    }
+
+    // register callbacks
+    renderer.setStreamAddedCallback(stream_added_callback, NULL);
+    renderer.setStreamRemovedCallback(stream_removed_callback, NULL);
+    renderer.setPositionCallback(position_changed_callback, NULL);
+    renderer.setTypeCallback(type_changed_callback, NULL);
+    renderer.setDisconnectCallback(render_disconnect_callback, NULL);
+
+    return a.exec();
+}
---
+++ src/render/samrenderer.h
@@ -0,0 +1,279 @@
+/**
+ * @file samrenderer.h
+ * SAM renderer library header
+ * @author Michelle Daniels
+ * @copyright UCSD 2014
+ * @license New BSD License: http://opensource.org/licenses/BSD-3-Clause
+ */
+
+#ifndef SAMRENDERER_H
+#define SAMRENDERER_H
+
+#include "libsamrender_global.h"
+#include "osc.h"
+
+/**
+ * @namespace sam
+ * Namespace for SAM-related functionality.
+ */
+namespace sam
+{
+
+static const unsigned int SAMRENDER_DEFAULT_TIMEOUT = 10000; // in milliseconds
+
+/**
+ * @enum SamRenderReturn
+ * The possible return codes for SamRenderer methods.
+ */
+enum SamRenderReturn
+{
+    SAMRENDER_SUCCESS = 0,    ///< Success
+    SAMRENDER_REQUEST_DENIED, ///< A SAM request was denied (i.e. registration or changing type failed)
+    SAMRENDER_NOT_REGISTERED, ///< Attempted to send a request to SAM before registering
+    SAMRENDER_OSC_ERROR,      ///< An error occurred trying to send an OSC message to SAM or receive one from SAM
+    SAMRENDER_TIMEOUT,        ///< A request to SAM timed out waiting for a response
+    SAMRENDER_ERROR,          ///< An error occurred that doesn't fit one of the above codes
+    SAMRENDER_NUM_RETURN_CODES
+};
+
+/**
+ * @struct SamRenderParams
+ * This struct contains the parameters needed to intialize a SamRenderer
+ */
+struct SamRenderParams
+{
+    SamRenderParams() :
+        samIP(NULL),
+        samPort(0),
+        replyIP(NULL),
+        replyPort(0)
+    {}
+
+    const char* samIP;          ///< IP address of SAM to connect to
+    quint16 samPort;            ///< Port on which SAM receives OSC messages
+    const char* replyIP;        ///< local IP address from which to send and receive OSC messages
+    quint16 replyPort;          ///< Local port for receiving OSC message replies (or 0 to have port assigned internally)
+};
+
+/**
+ * @struct SamRenderStream
+ * This struct contains the parameters describing a stream to be renderered.
+ */
+struct SamRenderStream
+{
+    SamRenderStream() :
+        id(0),
+        renderType(0),
+        renderPreset(0),
+        numChannels(0),
+        channels(NULL)
+    {}
+
+    int id;             ///< unique ID of this stream
+    int renderType;     ///< rendering type
+    int renderPreset;   ///< rendering preset
+    int numChannels;    ///< number of channels this stream contains
+    int* channels;      ///< array with numChannels elements, containing the indices of the input channels this stream will arrive on
+};
+
+/**
+ * @typedef StreamAddedCallback
+ * Prototype for a StreamAddedCallback function.
+ * This function is called whenever a new client stream is added to SAM.
+ * @param stream reference to SamRenderStream struct containing stream parameters
+ * @param arg pointer to user-supplied data
+ */
+typedef void(*StreamAddedCallback)(SamRenderStream& stream, void* arg);
+
+/**
+ * @typedef StreamRemovedCallback
+ * Prototype for a StreamRemovedCallback function.
+ * This function is called whenever a client stream is removed from SAM.
+ * @param id unique ID for the stream to be removed
+ * @param arg pointer to user-supplied data
+ */
+typedef void(*StreamRemovedCallback)(int id, void* arg);
+
+/**
+ * @typedef RenderPositionCallback
+ * Prototype for a SamRenderer position change callback function.
+ * This function is called when a stream's position changes
+ * @param id unique ID for the stream whose position changed
+ * @param x x-coordinate of stream
+ * @param y y-coordinate of stream
+ * @param width width of stream
+ * @param height height of stream
+ * @param depth depth of stream
+ * @param arg pointer to user-supplied data
+ */
+typedef void(*RenderPositionCallback)(int id, int x, int y, int width, int height, int depth, void* arg);
+
+/**
+ * @typedef RenderTypeCallback
+ * Prototype for a SamRenderer type change callback function.
+ * This function is called when the type or preset for a stream changes
+ * @param id the unique ID for the stream whose type/preset changed
+ * @param type the stream's type
+ * @param the stream's preset
+ * @param arg pointer to user-supplied data
+ */
+typedef void(*RenderTypeCallback)(int id, int type, int preset, void* arg);
+
+/**
+ * @typedef RenderDisconnectCallback
+ * Prototype for a SamRenderer disconect callback function.
+ * This function is called when the connection with SAM is broken.
+ * @param arg pointer to user-supplied data
+ */
+typedef void(*RenderDisconnectCallback)(void* arg);
+
+class SamRenderer : public QObject
+{
+    Q_OBJECT
+
+public:
+
+    /**
+      * SamRenderer default constructor.
+      * After construction, initialize using init()
+      */
+    SamRenderer();
+
+    /**
+     * SamRenderer destructor.
+     * @see SamRenderer
+     */
+    ~SamRenderer();
+
+    /**
+     * Copy constructor (not used).
+     */
+    SamRenderer(const SamRenderer&);
+
+    /**
+     * Assignment operator (not used).
+     */
+    SamRenderer& operator=(const SamRenderer);
+
+    /**
+     * Init this SamRenderer.
+     * This must be called before registering.
+     * @param params SamRenderParams struct of parameters
+     * @return 0 on success, a non-zero ::SamRenderReturn code on failure
+     */
+    int init(const SamRenderParams& params);
+
+    /**
+     * Register this renderer with SAM and block until response is received.
+     * @param timeout response timeout time in milliseconds
+     * @return 0 on success, a non-zero ::SamRenderReturn code on failure
+     */
+    int start(unsigned int timeout = SAMRENDER_DEFAULT_TIMEOUT);
+
+    /**
+     * Unregister this renderer from SAM.
+     * @return none
+     */
+    void stop();
+
+    /**
+     * Set stream added callback.
+     * @param callback the function this renderer will call when a stream is added
+     * @param arg each time the callback is called it will pass this as an argument
+     * @return 0 on success, a non-zero ::SamRenderReturn code on failure (will fail if a callback has already been set)
+     */
+    int setStreamAddedCallback(StreamAddedCallback callback, void* arg);
+
+    /**
+     * Set stream removed callback.
+     * @param callback the function this renderer will call when a stream is removed
+     * @param arg each time the callback is called it will pass this as an argument
+     * @return 0 on success, a non-zero ::SamRenderReturn code on failure (will fail if a callback has already been set)
+     */
+    int setStreamRemovedCallback(StreamRemovedCallback callback, void* arg);
+
+    /**
+     * Set position change callback.
+     * @param callback the function this renderer will call when a stream's position changes
+     * @param arg each time the callback is called it will pass this as an argument
+     * @return 0 on success, a non-zero ::SamRenderReturn code on failure (will fail if a callback has already been set)
+     */
+    int setPositionCallback(RenderPositionCallback callback, void* arg);
+
+    /**
+     * Set type change callback.
+     * @param callback the function this renderer will call when a stream's type or preset changes
+     * @param arg each time the callback is called it will pass this as an argument
+     * @return 0 on success, a non-zero ::SamRenderReturn code on failure (will fail if a callback has already been set)
+     */
+    int setTypeCallback(RenderTypeCallback callback, void* arg);
+
+    /**
+     * Set disconnect callback.
+     * @param callback the function this renderer will call if the connection with SAM is lost
+     * @param arg each time the callback is called it will pass this as an argument
+     * @return 0 on success, a non-zero ::SamRenderReturn code on failure (will fail if a callback has already been set)
+     */
+    int setDisconnectCallback(RenderDisconnectCallback callback, void* arg);
+
+public slots:
+    /**
+     * Handle an OSC message.
+     * @param msg the OSC message to handle
+     * @param sender the name of the host that sent the message
+     * @param socket the socket the message was received through
+     */
+    void handleOscMessage(OscMessage* msg, const char* sender, QAbstractSocket* socket);
+
+    /**
+     * Handle the case where we get disconnected from SAM.
+     */
+    void samDisconnected();
+
+signals:
+    /**
+     * Emitted when a response from SAM is received.
+     * This signal is for use internal to the SAC library, and clients don't need to (and shouldn't) connect it to any slots.
+     */
+    void responseReceived();
+
+private:
+
+    /**
+     * Handle a /sam/regconfirm OSC message.
+     */
+    void handle_regconfirm();
+
+    /**
+     * Handle a /sam/regdeny OSC message.
+     */
+    void handle_regdeny(int errorCode);
+
+    char* m_samIP;                    ///< SAM IP address
+    quint16 m_samPort;                ///< port on which SAM listens for OSC messages
+    bool m_registered;                ///< whether or not we've successfully registered with SAM
+
+    // for OSC
+    char* m_replyIP;
+    quint16 m_replyPort;              ///< OSC port for this renderer
+    QTcpSocket m_socket;              ///< Socket for sending and receiving OSC messages
+    bool m_responseReceived;          ///< whether or not SAM responded to request
+    OscTcpSocketReader* m_oscReader;  ///< OSC-reading helper
+
+    // for callbacks
+    StreamAddedCallback m_streamAddedCallback;      ///< the /sam/stream/add callback function
+    void* m_streamAddedCallbackArg;                 ///< user-supplied data for /sam/stream/add callback
+    StreamRemovedCallback m_streamRemovedCallback;  ///< the /sam/stream/remove callback function
+    void* m_streamRemovedCallbackArg;               ///< user-supplied data for /sam/stream/remove callback
+    RenderPositionCallback m_positionCallback;      ///< the /sam/val/position callback function
+    void* m_positionCallbackArg;                    ///< user-supplied data for /sam/val/position callback
+    RenderTypeCallback m_typeCallback;              ///< the /sam/val/type callback function
+    void* m_typeCallbackArg;                        ///< user-supplied data for /sam/val/type callback
+    RenderDisconnectCallback m_disconnectCallback;  ///< the disconnect callback function
+    void* m_disconnectCallbackArg;                  ///< user-supplied data for disconnect callback
+
+};
+
+} // end of namespace sam
+
+#endif // SAMRENDERER_H
